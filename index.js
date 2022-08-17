a1ddb03c9946992cdb3488115910d18cd5ca6def
이승원 <iseung-won@iseung-won-ui-MacBookPro.local>
2022-08-18 04:31:44 +0900

    22/08/22 When mk a token, error is occured!

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000;
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const { User } = require('./models/User');
 
 const config = require('./config/key');
@@ -29,6 +31,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 app.post('/register', (req, res) => {
   // After fetching some information from client to sign up
   // give database that information.
@@ -39,6 +43,35 @@ app.post('/register', (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post('./login', (req, res) => {
+  //Finding email requested from database
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: 'Invalid email',
+      });
+    }
+
+    //Confirming whether the password is same with the password in database
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: 'Invalid password' });
+
+      //Creating a token
+      user.genToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //Storing a token into cookie or local storage
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +33,46 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    // encoding a password
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
+
+userSchema.methods.genToken = function (cb) {
+  var user = this;
+
+  //Creating a token through jsonwebtoken
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  // user._id + 'secretTocken' = token
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
